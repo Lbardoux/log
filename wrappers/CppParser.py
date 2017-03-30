bbdf188e0e1a97b4b73f1c66a1f0803a0208060a
MTLCRBN <lbardoux@gmail.fr>
2017-03-30 11:53:36 +0200

    Creating the new architecture for Logs2.0

---
+++ wrappers/CppParser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#-*- encoding:utf-8 -*-
+
+# These script may parse logs.hpp and generate a datastructure usable to generate other language code.
+# In order to do so, respect the logs.hpp construction (with macro and shits).
+
+
+
+
